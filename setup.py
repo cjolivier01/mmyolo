1caa39dde7d17551c68942a5d0a632c433f1166d
Chris Olivier <cjolivier01@apache.org>
2025-11-29 10:36:20 -0800

    p13

--- setup.py
+++ setup.py
@@ -21,9 +21,16 @@ version_file = 'mmyolo/version.py'
 
 
 def get_version():
-    with open(version_file) as f:
-        exec(compile(f.read(), version_file, 'exec'))
-    return locals()['__version__']
+    version_ns = {}
+    with open(version_file, encoding='utf-8') as f:
+        exec(compile(f.read(), version_file, 'exec'), version_ns)
+    import sys
+
+    # return short version for sdist
+    if 'sdist' in sys.argv or 'bdist_wheel' in sys.argv:
+        return version_ns['short_version']
+    else:
+        return version_ns['__version__']
 
 
 def parse_requirements(fname='requirements.txt', with_version=True):
